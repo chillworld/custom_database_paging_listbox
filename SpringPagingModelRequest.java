f35b121d4051732c5d71a8a25115aa7deda90929
chillworld <langefilip@yahoo.com>
2015-11-18 12:08:46 +0100

    Update SpringPagingModelRequest.java

--- SpringPagingModelRequest.java
+++ SpringPagingModelRequest.java
@@ -1,3 +1,5 @@
+package be.mil.components;
+
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.lang.StringUtils;
@@ -5,15 +7,23 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Order;
+import org.zkoss.zk.ui.WrongValueException;
 
 /**
  * Abstract implementation for Spring paging. Required to implement the method
  * "public Page getPage(PageRequest request);"
  *
  * If you want to have multiple sortfields, you have to seperate them in zul
- * page with ; example for ZUL :  <listheader sort="client(id;name)/>
- * In this example the sortage is first on id, second on name.
- * Sorts are ignorecase!!!!
+ * page with ; or , or a space example for ZUL :  <listheader sort="client(id;name,birthdate)/>
+ * 
+ * Sortdirection can be forced with ASC, DESC or INVERSE.
+ * Example : <listheader sort="client(id;inverse(name))/>
+ * If id sortage is ASC name will be sorted secondly DESC.
+ * Example : <listheader sort="client(name,desc(birthdate))/>
+ * Birthdate will ALWAYS be sorted DESC but the sortage of name changes when pressing sort.
+ * 
+ * Ignorecase can be asked with LOWER or UPPER.
+ * Example : <listheader sort="client(lower(name))/>
  *
  * @author cossaer.f
  */
@@ -22,7 +32,7 @@ public abstract class SpringPagingModelRequest implements PagingModelRequest {
     private long totalSize;
 
     @Override
-    public List getPage(int activePage, int pageSize, String sortField, SortDirection sortDirection) {
+    public List getContent(int activePage, int pageSize, String sortField, SortDirection sortDirection) {
         PageRequest request;
         Sort.Direction direction;
         if (sortDirection != null) {
@@ -32,8 +42,8 @@ public abstract class SpringPagingModelRequest implements PagingModelRequest {
         }
         if (!StringUtils.isEmpty(sortField)) {
             List<Order> sortOrders = new ArrayList<>();
-            for (String field : sortField.split(";")) {
-                sortOrders.add(new Sort.Order(direction, field).ignoreCase());
+            for (String field : sortField.split("(\\s*,\\s*)|(\\s*;\\s*)|(\\s+)")) {// split on (',' or ';' or space) with leading or ending spaces.
+                sortOrders.add(createSortOrder(field, direction));
             }
             request = new PageRequest(activePage, pageSize, new Sort(sortOrders));
         } else {
@@ -50,4 +60,48 @@ public abstract class SpringPagingModelRequest implements PagingModelRequest {
     }
 
     public abstract Page getPage(PageRequest request);
+
+    private Order createSortOrder(String sortField, Sort.Direction direction) {
+        String[] splitted = sortField.split("(\\()");
+        Sort.Direction sortDirection = direction;
+        boolean ignoreCase = false;
+        String field = "";
+        for (int counter = 0; counter < splitted.length; counter++) {
+            if (counter != splitted.length - 1) {
+                try {
+                    FieldBehavior behavior = FieldBehavior.valueOf(splitted[counter].toUpperCase());
+                    switch (behavior) {
+                        case LOWER:
+                        case UPPER:
+                            ignoreCase = true;
+                            break;
+                        case ASC:
+                            sortDirection = Sort.Direction.ASC;
+                            break;
+                        case DESC:
+                            sortDirection = Sort.Direction.DESC;
+                            break;
+                        case INVERSE:
+                            sortDirection = direction == Sort.Direction.ASC ? Sort.Direction.DESC : Sort.Direction.ASC;
+                    }
+
+                } catch (IllegalArgumentException ex) {
+                    throw new WrongValueException("Value : " + splitted[counter] + " is not a predifined value.\nAllowed values are : ASC, DESC, INVERSE, UPPER, LOWER");
+                }
+            } else {
+                field = splitted[counter].replace(")", "");
+            }
+        }
+        Order order = new Order(sortDirection, field);
+        return ignoreCase?order.ignoreCase():order;
+    }
+
+    private enum FieldBehavior {
+
+        ASC,
+        DESC,
+        INVERSE,
+        LOWER,
+        UPPER;
+    }
 }
